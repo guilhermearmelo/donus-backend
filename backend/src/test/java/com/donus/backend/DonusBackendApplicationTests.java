67e6d4beb0923414cfd5fab8f12c3641b61d6202
Guilherme A. Melo <guilhermearmelo@gmail.com>
2022-03-08 22:17:18 -0300

    #001 - Start basic maven project

---
+++ backend/src/test/java/com/donus/backend/DonusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
