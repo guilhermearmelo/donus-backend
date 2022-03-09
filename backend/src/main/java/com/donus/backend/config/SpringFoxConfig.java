996d3766068bb207b227e78873939a2b97e7c2aa
Guilherme A. Melo <guilhermearmelo@gmail.com>
2022-03-09 17:58:23 -0300

    #006 - Adding Swagger Documentation
    Downgrade SpringBoot parent to prevent Springfox 2.9.2 from crashing

---
+++ backend/src/main/java/com/donus/backend/config/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package com.donus.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("Donus")
+                        .description("Donus - API REST")
+                        .version("0.0.1-SNAPSHOT")
+                        .contact(new Contact("Guilherme Melo", "https://www.linkedin.com/in/guilhermearmelo/", "guilhermearmelo@gmail.com"))
+                        .build());
+    }
+}
